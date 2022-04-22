a62c543957ca452da0fef86f8ceb3d404cccdaaf
Mohammed <mohammja@metropolia.fi>
2022-04-22 14:16:37 +0300

    1. muuttaja

--- app/src/main/java/fi/ottooks/dreamcatcherdemo/UserInputs.java
+++ app/src/main/java/fi/ottooks/dreamcatcherdemo/UserInputs.java
@@ -3,10 +3,12 @@ package fi.ottooks.dreamcatcherdemo;
 public class UserInputs {
 
     private String date;
+    private int startTime;
 
     public UserInputs() {
 
         this.date = "";
+        this.startTime = 0;
 
     }
 
