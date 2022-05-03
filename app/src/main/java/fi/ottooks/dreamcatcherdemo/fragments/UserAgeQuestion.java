5fc934c269374a2d1f412981ab8caa0bc4abd3ac
Mohammed <mohammja@metropolia.fi>
2022-05-03 16:04:23 +0300

    iän tiedot fragmentti on valmis

--- app/src/main/java/fi/ottooks/dreamcatcherdemo/fragments/UserAgeQuestion.java
+++ app/src/main/java/fi/ottooks/dreamcatcherdemo/fragments/UserAgeQuestion.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import fi.ottooks.dreamcatcherdemo.R;
 
@@ -50,17 +53,53 @@ public class UserAgeQuestion extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_user_age_question, container, false);
+        EditText userAge = view.findViewById(R.id.userAge);
+        TextView ageInfoTv = view.findViewById(R.id.ageInfo);
+
+        Button ageBtn = view.findViewById(R.id.ageBtn);
+        ageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userAge.getText().toString().trim().length() != 0) {
+                    int age = Integer.parseInt(userAge.getText().toString());
+                    //tiedot ovat Sleep Foundation netti sivulta.
+                    if (age > 0 && age < 3) {
+                        ageInfoTv.setText("Suositelttu uniaika vauvallesi: 11-14 tuntia");
+                    } else if (age > 2 && age < 6) {
+                        ageInfoTv.setText("Suositeltu uniaika lapsellesi: 10-13 tuntia.");
+                    } else if (age > 5 && age < 10) {
+                        ageInfoTv.setText("suositeltu uniaika lapsellesi: 9-11 tuntia.");
+                    } else if (age > 9 && age < 14) {
+                        ageInfoTv.setText("Suositeltu uniaika sinulle: 9-11 tuntia.");
+                    } else if (age > 13 && age < 18) {
+                        ageInfoTv.setText("Suositeltu uniaika sinulle: 8-10 tuntia.");
+                    } else if (age > 17 && age < 65) {
+                        ageInfoTv.setText("Suositeltu uniaika sinulle: 7-9 tuntia.");
+                    } else if (age >= 65) {
+                        ageInfoTv.setText("Suositeltu unia aika sinulle: 7-8 tuntia.");
+                    } else {
+                        ageInfoTv.setText("Ikä pitäisi olla enemmän kuin 1.");
+                    }
+                }
+
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_age_question, container, false);
+        return view ;
     }
 }
